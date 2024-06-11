c1255af00098171768632ee2fbff8ba26029457a
vskus <vskus@beyond.ai>
2024-06-11 15:08:39 +0300

    made car bigger

--- src/app/componenst/Movie/Movie.tsx
+++ src/app/componenst/Movie/Movie.tsx
@@ -7,15 +7,16 @@ interface IProps {
 
 export default function Movie({ movie }: IProps) {
     return (
-        <section className="bg-white rounded-lg shadow-md p-4 max-w-xs w-full h-96">
-            <div className="w-full h-60 bg-gray-200 rounded-t-lg overflow-hidden">
+        <section className="bg-white rounded-lg shadow-md p-4 max-w-xs w-full h-112"> {/* Adjust the height here */}
+            <div
+                className="w-full h-72 bg-gray-200 rounded-t-lg overflow-hidden"> {/* Adjust the height of the image container */}
                 <img
                     src={movie.Poster}
                     alt={`${movie.Title} Poster`}
                     className="w-full h-full object-cover"
                 />
             </div>
-            <div className="p-4 flex flex-col justify-between h-36">
+            <div className="p-4 flex flex-col justify-between h-40"> {/* Adjust the height here */}
                 <div>
                     <h2 className="text-lg font-bold mb-2">{movie.Title}</h2>
                     <p className="text-sm text-gray-600">Year: {movie.Year}</p>
