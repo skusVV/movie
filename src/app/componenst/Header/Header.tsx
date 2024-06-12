db5dad5865e019964c55b7b134ca0e6d9d57360f
vskus <vskus@beyond.ai>
2024-06-12 18:23:08 +0300

    added home button

--- src/app/componenst/Header/Header.tsx
+++ src/app/componenst/Header/Header.tsx
@@ -1,12 +1,16 @@
+import { useRouter } from 'next/navigation';
+
 interface IHeaderProps {
     searchTerm: string;
     setSearchTerm: (query: string) => void;
 }
 
 export default function Header({ searchTerm, setSearchTerm }: IHeaderProps) {
+    const router = useRouter();
 
     return (
-        <header className="bg-gray-900 p-4 shadow-md">
+        <header className="bg-gray-900 p-4 shadow-md flex items-center">
+            <h1 className="ml-4 text-xl font-bold text-cyan-50 cursor-pointer" onClick={() => router.replace('/')}>M</h1>
             <div className="container mx-auto">
                 <input
                     type="text"
